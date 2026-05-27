fa96050cd8a9b82252cacf9f78de3df6b95e911a
rKafevas <kfv2@discente.ifpe.edu.br>
2026-05-27 11:22:41 -0300

    fix: adicionando ssl na conexao com o banco

--- src/database/index.js
+++ src/database/index.js
@@ -2,9 +2,13 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
   ssl: {
-    rejectUnauthorized: false
+    rejectUnauthorized: false // Garante que o Render consiga se conectar ao Neon com segurança
   }
 });
 
